f220a70d390cb813f8b513be581025ae3d1079ff
Kaustubh J <kaustubhkj@gmail.com>
2020-11-18 23:07:06 +0530

    Update index.js

--- src/search/index.js
+++ src/search/index.js
@@ -1,6 +1,7 @@
 const kmp = require('./kmp');
 const rabinKarp = require('./rabin_karp');
-const AhoCorasick = require('./AhoCorasick')
+const AhoCorasick = require('./AhoCorasick');
+
 module.exports = {
   AhoCorasick,
   kmp,
