a8397501fd1702b8b917f1453705b380d2eb771e
Kukulkan <kaustubhkj@gmail.com>
2020-11-29 20:49:15 +0530

    minor update

---
+++ src/sort/index.js
@@ -0,0 +1,9 @@
+const burstSort = require("./burstSort");
+const customSort = require("./stringSort").sort;
+const { sorted } = require("./stringSort");
+
+module.exports = {
+  burstSort,
+  customSort,
+  sorted,
+};
